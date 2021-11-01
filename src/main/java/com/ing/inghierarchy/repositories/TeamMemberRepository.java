aaf9f7f237f3ed55fdb11508031568494f1ab5c6
Ryan van den Bogaard <bogaardryan@hotmail.com>
2021-11-01 09:30:03 +0100

    Role + Test data (#3)

--- src/main/java/com/ing/inghierarchy/repositories/TeamMemberRepository.java
+++ src/main/java/com/ing/inghierarchy/repositories/TeamMemberRepository.java
@@ -1,10 +1,13 @@
 package com.ing.inghierarchy.repositories;
 
 import com.ing.inghierarchy.domain.TeamMember;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamMemberRepository extends MongoRepository<TeamMember, String>, TeamMemberRepositoryCustom {
-
+	List<TeamMember> findByRoleId(String roleId);
 }
