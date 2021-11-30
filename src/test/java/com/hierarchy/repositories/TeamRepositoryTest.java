1afe9404adcf94ecdd1f8a3c7595fd9614ea4e2d
Adrian Nachev <Adrian.Nachev@ing.com>
2021-11-30 09:51:00 +0000

    Merged PR 320279: Fix build
    Related work items: #869791

--- src/test/java/com/hierarchy/repositories/TeamRepositoryTest.java
+++ src/test/java/com/hierarchy/repositories/TeamRepositoryTest.java
@@ -99,4 +99,79 @@ class TeamRepositoryTest {
                 .containsExactlyInAnyOrder("team3-id");
         assertThat(teamTypeNoResults).isEmpty();
     }
+
+    @Test
+    void findAllByLeadId() {
+
+        // Prepare
+        var team1 = team("team 1", null, "1", null).setId("team1-id");
+        var team2 = team("team 2", null, "1", null).setId("team2-id");
+        var team3 = team("team 3", null, "2", null).setId("team3-id");
+        teamRepository.saveAll(List.of(team1, team2, team3));
+
+        // Test
+        List<Team> leadId1Results = teamRepository.findAllByLeadId("1");
+        List<Team> leadId2Results = teamRepository.findAllByLeadId("2");
+        List<Team> leadIdNoResults = teamRepository.findAllByLeadId("no-results");
+
+        // Verify
+        assertThat(leadId1Results).hasSize(2);
+        assertThat(leadId1Results.stream().map(Team::getId).collect(toList()))
+                .containsExactlyInAnyOrder("team1-id", "team2-id");
+        assertThat(leadId2Results).hasSize(1);
+        assertThat(leadId2Results.stream().map(Team::getId).collect(toList()))
+                .containsExactlyInAnyOrder("team3-id");
+        assertThat(leadIdNoResults).isEmpty();
+    }
+
+    @Test
+    void existsByManagedBy() {
+
+        // Prepare
+        var team = team("team 1", "1", "1", null).setId("team1-id");
+        teamRepository.save(team);
+
+        // Test & Verify
+        assertThat(teamRepository.existsByManagedBy("1")).isTrue();
+        assertThat(teamRepository.existsByManagedBy("non-existent")).isFalse();
+    }
+
+    @Test
+    void countAllByTeamType() {
+
+        // Prepare
+        var team1 = team("team 1", null, null, "1");
+        var team2 = team("team 2", null, null, "1");
+        var team3 = team("team 3", null, null, "2");
+        teamRepository.saveAll(List.of(team1, team2, team3));
+
+        // Test && Verify
+        assertThat(teamRepository.countAllByTeamType("1")).isEqualTo(2);
+        assertThat(teamRepository.countAllByTeamType("2")).isEqualTo(1);
+        assertThat(teamRepository.countAllByTeamType("non-existent")).isEqualTo(0);
+    }
+
+    @Test
+    void fetchTeamsWithPaginationByTeamType() {
+
+        // Prepare
+        var team1 = team("team 1", null, null, "1").setId("team1-id");
+        var team2 = team("team 2", null, null, "1").setId("team2-id");
+        var team3 = team("team 3", null, null, "2").setId("team3-id");
+        teamRepository.saveAll(List.of(team1, team2, team3));
+
+        // Test
+        List<Team> leadId1Results = teamRepository.fetchTeamsWithPaginationByTeamType("1", 1, 1);
+        List<Team> leadId2Results = teamRepository.fetchTeamsWithPaginationByTeamType("2", 1, 0);
+        List<Team> leadIdNoResults = teamRepository.fetchTeamsWithPaginationByTeamType("2", 1, 1);
+
+        // Verify
+        assertThat(leadId1Results).hasSize(1);
+        assertThat(leadId1Results.stream().map(Team::getId).collect(toList()))
+                .containsExactlyInAnyOrder("team2-id");
+        assertThat(leadId2Results).hasSize(1);
+        assertThat(leadId2Results.stream().map(Team::getId).collect(toList()))
+                .containsExactlyInAnyOrder("team3-id");
+        assertThat(leadIdNoResults).isEmpty();
+    }
 }
